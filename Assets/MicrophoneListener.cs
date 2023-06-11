b43cd9d819537694b90ac7965c4685f724af9375
passion <563129582@qq.com>
2023-06-12 00:37:45 +0800

    实现mic监听声音fft

--- Assets/MicrophoneListener.cs
+++ Assets/MicrophoneListener.cs
@@ -1,34 +1,49 @@
-﻿using UnityEngine;
+﻿using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.Serialization;
 
 [RequireComponent(typeof(AudioSource))]
 public class MicrophoneListener : MonoBehaviour
 {
-    public GameObject[] obj;
-
     public bool startMicOnStartup = true;
     public bool stopMicrophoneListener = false;
     public bool startMicrophoneListener = false;
     private bool microphoneListenerOn = false;
 
-    AudioSource src;
-    float timeSinceRestart = 0;
-    public float threshold = 5;
-    private float[] spectrumData = new float[128];
+    private AudioSource m_AudioSouce;
+    private float timeSinceRestart = 0;
+    private float[] spectrumData = new float[512];
+    private float[] _fragBand = new float[8];
+
+    public GameObject[] obj;
+    public int scaleMultiplier = 100;
+    public float threhold = 10;
+    public int channelCount = 1;
+    [FormerlySerializedAs("frames")] public int frameCount = 60;
+    public UnityEvent<int[]> FrenquencyEvent;
+
+    private int[] frenquncies = new int[8] { 0, 0, 0, 0, 0, 0, 0, 0 };
+    private int frame = 0;
 
     void Start()
     {
+        Application.targetFrameRate = 60;
         if (startMicOnStartup)
         {
             RestartMicrophoneListener();
             StartMicrophoneListener();
         }
     }
+
     void Update()
     {
         if (stopMicrophoneListener)
         {
             StopMicrophoneListener();
         }
+
         if (startMicrophoneListener)
         {
             StartMicrophoneListener();
@@ -39,20 +54,42 @@ public class MicrophoneListener : MonoBehaviour
 
         //创建Microphone实例
         MicrophoneIntoAudioSource(microphoneListenerOn);
+
         //处理音频
         ProcessSound();
-        //处理波峰
-        ShowPeaks();
+        ShowBox();
+
+        var arr = FindTopTwoIndices(_fragBand, threhold);
+        for (int i = 0; i < channelCount; i++)
+        {
+            if (arr[i] > -1)
+                frenquncies[arr[i]]++;
+        }
+
+        frame++;
+        if (frame >= frameCount)
+        {
+            frame = 0;
+            int[] arr2 = FindTopTwoIndices(frenquncies);
+            for (int i = 0; i < frenquncies.Length; i++)
+            {
+                frenquncies[i] = 0;
+            }
+
+            FrenquencyEvent?.Invoke(arr2);
+        }
     }
+
     //停止录制
     public void StopMicrophoneListener()
     {
         microphoneListenerOn = false;
-        src.Stop();
-        src.clip = null;
+        m_AudioSouce.Stop();
+        m_AudioSouce.clip = null;
 
         Microphone.End(null);
     }
+
     //开始录制
     public void StartMicrophoneListener()
     {
@@ -63,48 +100,94 @@ public class MicrophoneListener : MonoBehaviour
     //处理声音数据，通过FFT算法
     public void ProcessSound()
     {
-        src.GetSpectrumData(spectrumData, 0, FFTWindow.BlackmanHarris);
+        m_AudioSouce.GetSpectrumData(spectrumData, 0, FFTWindow.BlackmanHarris);
 
-        for (int i = 0; i < 128; i++)
+        int count = 0;
+        for (int i = 0; i < 8; i++)
         {
-            var y = spectrumData[i] * 10000;
-            y = Mathf.Clamp(y, 0, 10);
-            obj[i].gameObject.transform.localScale = new Vector3(0.3f, y, 0.5f); //将可视化的物体和音波相关联
-            
-            //设定阈值，低于代表环境音，可以忽略
-            if (y < threshold)
+            float average = 0;
+            int sampleCount = (int)Mathf.Pow(2, i) * 2;
+            if (i == 7)
             {
-                spectrumData[i] = 0;
+                sampleCount += 2;
             }
+
+            for (int j = 0; j < sampleCount; j++)
+            {
+                average += spectrumData[count] * (count + 1);
+                count++;
+            }
+
+            average /= count;
+            _fragBand[i] = average * 10;
         }
     }
 
-    void ShowPeaks()
+    void ShowBox()
     {
-        for (int i = 0; i < 128; i++)
+        for (int i = 0; i < 8; i++)
         {
-            var y = spectrumData[i] * 10000;
-            //设定阈值，低于代表环境音，可以忽略
-            if (y < threshold)
+            var y = _fragBand[i] * scaleMultiplier;
+            obj[i].gameObject.transform.localScale = new Vector3(0.3f, y, 0.5f); //将可视化的物体和音波相关联
+        }
+    }
+
+    int[] FindTopTwoIndices(float[] arr, float threhold)
+    {
+        int maxIndex = -1;
+        int secondMaxIndex = -1;
+        float maxValue = int.MinValue;
+        float secondMaxValue = int.MinValue;
+
+        for (int i = 0; i < arr.Length; i++)
+        {
+            if (arr[i] > maxValue && arr[i] * scaleMultiplier > threhold)
             {
-                spectrumData[i] = 0;
+                secondMaxValue = maxValue;
+                secondMaxIndex = maxIndex;
+                maxValue = arr[i];
+                maxIndex = i;
+            }
+            else if (arr[i] > secondMaxValue && arr[i] * scaleMultiplier > threhold)
+            {
+                secondMaxValue = arr[i];
+                secondMaxIndex = i;
             }
         }
-        
-        //计算波峰数量
-        float total = 0;
-        var peaks = FindPeaks.getPeaks(spectrumData);
-        for (int i = 0; i < peaks.Length; i++)
+
+        return new[] { maxIndex, secondMaxIndex };
+    }
+
+    static int[] FindTopTwoIndices(int[] arr)
+    {
+        int maxIndex = -1;
+        int secondMaxIndex = -1;
+        int maxValue = int.MinValue;
+        int secondMaxValue = int.MinValue;
+
+        for (int i = 0; i < arr.Length; i++)
         {
-            total += spectrumData[peaks[i]];
+            if (arr[i] > maxValue)
+            {
+                secondMaxValue = maxValue;
+                secondMaxIndex = maxIndex;
+                maxValue = arr[i];
+                maxIndex = i;
+            }
+            else if (arr[i] > secondMaxValue)
+            {
+                secondMaxValue = arr[i];
+                secondMaxIndex = i;
+            }
         }
-        obj[127].transform.localScale = new Vector3(0.3f, peaks.Length, 0.5f);
+
+        return new[] { maxIndex, secondMaxIndex };
     }
 
     public void RestartMicrophoneListener()
     {
-        src = GetComponent<AudioSource>();
-        src.clip = null;
+        m_AudioSouce = GetComponent<AudioSource>();
+        m_AudioSouce.clip = null;
         timeSinceRestart = Time.time;
     }
 
@@ -114,13 +197,16 @@ public class MicrophoneListener : MonoBehaviour
         {
             if (Time.time - timeSinceRestart > 0.5f && !Microphone.IsRecording(null))
             {
-                src.clip = Microphone.Start(null, true, 999, 44100);
-                while (!(Microphone.GetPosition(null) > 0)){}
-                src.Play(); // Play the audio source
+                m_AudioSouce.clip = Microphone.Start(null, true, 999, 44100);
+                while (!(Microphone.GetPosition(null) > 0))
+                {
+                }
+
+                m_AudioSouce.Play(); // Play the audio source
             }
         }
     }
-    
+
     private void OnAudioFilterRead(float[] data, int channels)
     {
         for (int i = 0; i < data.Length; i++)
